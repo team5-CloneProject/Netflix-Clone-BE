a608c2086ab6ae46e7613b4f3748a7266a4cf6b2
JaeHun <clickyour@gmail.com>
2023-03-04 09:40:02 +0900

    feat : #22 API 에러 핸들링
    UnknownHostException, HttpClientErrorException에 대해
    에러 핸들링 추가

--- src/main/java/com/sparta/netflixclone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/netflixclone/exception/GlobalExceptionHandler.java
@@ -5,17 +5,20 @@ package com.sparta.netflixclone.exception;
 import com.sparta.netflixclone.common.ApiResponseDto;
 import com.sparta.netflixclone.common.ErrorResponse;
 import com.sparta.netflixclone.common.ResponseUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.net.UnknownHostException;
 
 @RestController
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-//    protected ResponseEntity<ApiResponseDto<ErrorResponse>> customException(CustomException e){
         protected ResponseEntity<ApiResponseDto<ErrorResponse>> customException(CustomException e){
 
 //        return ErrorResponse.toResponseEntity(e.getExceptionEnum());
@@ -26,4 +29,25 @@ public class GlobalExceptionHandler {
 
     }
 
+    @ExceptionHandler(UnknownHostException.class)
+    protected ResponseEntity<ApiResponseDto<ErrorResponse>> customException(UnknownHostException e){
+
+//        return ErrorResponse.toResponseEntity(e.getExceptionEnum());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .body(ResponseUtils.error(
+                        ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage())
+                ));
+
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected ResponseEntity<ApiResponseDto<ErrorResponse>> customException(HttpClientErrorException e){
+
+//        return ErrorResponse.toResponseEntity(e.getExceptionEnum());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+                .body(ResponseUtils.error(
+                        ErrorResponse.of(HttpStatus.NOT_FOUND.value(),e.getMessage())
+                ));
+
+    }
 }
